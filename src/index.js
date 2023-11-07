a27119440384b8a3dd2d3abde8ba636e96f5327b
Kylan1940 <kevingaming123copyright@gmail.com>
2023-11-07 09:34:00 +0700

    added kick and timeout commands

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, ActivityType } = require('discord.js');
 const eventHandler = require('./handlers/eventHandler');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
+
+let status = [
+  {
+    name: 'web.kylan1940.site',
+    type: ActivityType.Watching,
+  },
+];
+
+client.on('ready', (c) => {
+  console.log(`✅ ${c.user.tag} is online.`);
+
+  setInterval(() => {
+    let random = Math.floor(Math.random() * status.length);
+    client.user.setActivity(status[random]);
+  }, 10000);
+});
 
 eventHandler(client);
 
